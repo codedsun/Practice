507e051b7fe207c8b571e5b2b427a832758d24bc
Suneet Srivastava <suneetbond91@gmail.com>
2018-01-12 13:52:04 +0530

    Initial commit

---
+++ app/src/main/java/com/suneetsrivastava/practice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.suneetsrivastava.practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Recyc
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/suneetsrivastava/practice/ViewModel/DataViewModel.java
@@ -0,0 +1,10 @@
+package com.suneetsrivastava.practice.ViewModel;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by suneetsrivastava on 12/01/18.
+ */
+
+public class DataViewModel extends ViewModel {
+}
